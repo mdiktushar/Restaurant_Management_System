412405887ee67354812a65afe5e6d630858a004b
mdiktushar <md.tushar1804@gmail.com>
2021-11-15 00:04:16 +0600

    (3) User Registration,  Login, Logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,6 +83,28 @@ https://templatemo.com/tm-558-klassy-cafe
                             </li>
                             <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
                             <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
+                        
+                            <li>
+                            @if (Route::has('login'))
+                                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                @auth
+                                <li> 
+                                    <x-app-layout/>
+                                </li>    
+                            @else
+                                <li>
+                                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li>
+                                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                    </li>
+                                @endif
+                                @endauth
+                                </div>
+                            @endif  
+
+                            </li>
                         </ul>        
                         <a class='menu-trigger'>
                             <span>Menu</span>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get("/home",[HomeController::class, "index"]);
+Route::get('',[HomeController::class, 'index']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
